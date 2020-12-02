da069e04b581de292468ec6e0c490d07d1658c6b
Anwesh43 <anweshthecool0@gmail.com>
2020-12-03 00:17:17 +0530

    adding constants and libraries

--- rectuprotlineview/src/main/java/com/example/rectuprotlineview/RectUpRotLineView.kt
+++ rectuprotlineview/src/main/java/com/example/rectuprotlineview/RectUpRotLineView.kt
@@ -1,2 +1,27 @@
 package com.example.rectuprotlineview
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val parts : Int = 4
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.8f
+val delay : Long = 20
+val colors : Array<Int> = arrayOf(
+    "#F44336",
+    "#009688",
+    "#03A9F4",
+    "#FF9800",
+    "#3F51B5"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val rot : Float = 90f
+
